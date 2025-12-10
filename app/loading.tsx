1c400338e3e15628c091ca2bfc7fb3ffac2c132a
gentlehacks <salihuadamu4678@gmail.com>
2025-12-10 13:36:26 +0100

    Fix errors

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Loading = () => {
+  return (
+    <div className="w-full h-scree flex items-center justify-center">
+      <div className="w-10 h-10 rounded-full bg-blue-600 animate-ping animate-bounce" />
+    </div>
+  )
+}
+
+export default Loading
