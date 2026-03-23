bd63208cc8be66f37327da56eef19b741ba85fa4
gentlehacks <salihuadamu4678@gmail.com>
2026-03-23 06:49:08 +0100

    Update: About gallery image to be responsive

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,6 @@ import { RiMapPinUserFill } from "react-icons/ri"
 
 const images = [
   "image-1.JPG",
-  "image-2.PNG",
   "image-3.JPG",
   "image-4.jpg"
 ]
@@ -177,8 +176,8 @@ const About = () => {
           animate={{ opacity: 1,}}
           exit={{ opacity: 0, }}
           transition={{ duration: 0.2, ease: "easeInOut" }}
-          className="h-fit max-h-180 md:max-h-200 lg:max-h-130 xl:max-h-180 w-full md:w-[70%] lg:w-[42%] mt-12 md:mt-0 p-2 rounded-2xl overflow-hidden flex items-center justify-center bg-gray-700/20 backdrop-filter backdrop-blur-md border border-gray-700">
-          <div className="overflow-hidden min-h-180 rounded-xl">
+          className="h-fit w-full md:w-[70%] lg:w-[42%] mt-12 md:mt-0 p-2 rounded-2xl overflow-hidden flex items-center justify-center bg-gray-700/20 backdrop-filter backdrop-blur-md border border-gray-700">
+          <div className="overflow-hidden  rounded-xl">
             <Image
               alt="GentleManDev Image"
               src={`/${images[imageIndex]}`}
