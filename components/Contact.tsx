c3cbc5f8dfbb0d25b4cb91e0fc4e84f10ba92f29
Gentleman <salihuadamu4678@gmail.com>
2026-03-29 07:43:20 +0100

    Update GitHub link to X.com in Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -85,7 +85,7 @@ export default function Contact() {
               <Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/salihu-adamu-3b9b97290" className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-gray-700 hover:bg-blue-500 transition-all duration-200">
                 <LuLinkedin size={20} />
               </Link>
-              <Link target="_blank" rel="noopener noreferrer" href="https://github.com/gentlemanwebdev" className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-gray-700 hover:bg-blue-500 transition-all duration-200">
+              <Link target="_blank" rel="noopener noreferrer" href="https://x.com/gentlemanwebdev" className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-gray-700 hover:bg-blue-500 transition-all duration-200">
                 <LuTwitter size={20} />
               </Link>
               <Link target="_blank" rel="noopener noreferrer" href="https://tiktok.com/@gentlemandev" className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-gray-700 hover:bg-blue-500 transition-all duration-200">
